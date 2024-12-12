69d0eede72e89b67326250b21cc31d4a919f9ae2
donfortune <donfortunet.df@gmail.com>
2024-12-12 09:09:50 +0100

    paramertize python script to handle different scan types and file types

--- import_scan.py
+++ import_scan.py
@@ -1,6 +1,23 @@
 import requests
 import os
 from dotenv import load_dotenv
+import sys #read user input
+
+file_name = sys.argv[1] #get the file name from the user input
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'nmap.json':
+    scan_type = 'Nmap Scan'
+elif file_name == 'njsscan.serif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+else:
+    print('Invalid file name')
+    sys.exit()
+
 
 load_dotenv()
 
@@ -14,7 +31,7 @@ headers = {
 url = 'https://demo.defectdojo.org/api/v2/import-scan/'
 
 data = {
-    'scan_type': 'Gitleaks Scan',
+    # 'scan_type': 'Gitleaks Scan',
     'active': 'true',
     'verified': 'true',
     'minimum_severity': 'low',
@@ -31,3 +48,4 @@ if response.status_code == 201:
 else:
     print('Failed to import scan report')
     print(response.content)
+
